5a65c53bf38897622e36a0c19e3e45c97bfe83e7
dexerq <114652232+dexerq@users.noreply.github.com>
2023-10-24 23:27:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 APP_ID = os.environ.get("APP_ID", "12706503")
 APP_HASH = os.environ.get("APP_HASH", "4344a75f9ae9d1d03f577b61e7313884")
 SESSION = os.environ.get("SESSION")
-ANTi = TelegramClient(StringSession(SESSION), APP_ID, APP_HASH)
-ANTi.start()
+tt = TelegramClient(StringSession(SESSION), APP_ID, APP_HASH)
+tt.start()
