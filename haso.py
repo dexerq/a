79e4e38f886c7262bec08f65fe6587808dbb3ffc
dexerq <114652232+dexerq@users.noreply.github.com>
2023-10-24 23:44:46 +0300

    Update haso.py

--- haso.py
+++ haso.py
@@ -9,7 +9,7 @@ from config import *
 from asyncio import sleep
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(name)s:%(message)s')
-logger = logging.getLogger("ANTi")
+logger = logging.getLogger("tt")
 logger.info("النشر التلقائي شغال الان استمتع ✓")
 
 yaAli = False
@@ -18,11 +18,11 @@ async def aljoker_nshr(tt, sleeptimet, chat, message, seconds):
     yaAli = True
     while yaAli:
         if message.media:
-            sent_message = await ANTi.send_file(chat, message.media, caption=message.text)
+            sent_message = await tt.send_file(chat, message.media, caption=message.text)
         else:
-            sent_message = await ANTi.send_message(chat, message.text)
+            sent_message = await tt.send_message(chat, message.text)
         await asyncio.sleep(sleeptimet)
-@ANTi.on(events.NewMessage(outgoing=True, pattern=r"^\.نشر (\d+) (@?\S+)$"))
+@tt.on(events.NewMessage(outgoing=True, pattern=r"^\.نشر (\d+) (@?\S+)$"))
 async def Hussein(event):
     await event.delete()
     parameters = re.split(r'\s+', event.text.strip(), maxsplit=2)
@@ -30,14 +30,14 @@ async def Hussein(event):
         return await event.reply("⌔∮ يجب استخدام كتابة صحيحة الرجاء التاكد من الامر اولا ⚠️")
     seconds = int(parameters[1])
     chat_usernames = parameters[2].split()
-    ANTi = event.client
+    tt = event.client
     global yaAli
     yaAli = True
     message = await event.get_reply_message()
     for chat_username in chat_usernames:
         try:
-            chat = await ANTi.get_entity(chat_username)
-            await aljoker_nshr(ANTi, seconds, chat.id, message, seconds)  # تمرير قيمة seconds هنا لكل مجموعة
+            chat = await tt.get_entity(chat_username)
+            await aljoker_nshr(tt, seconds, chat.id, message, seconds)  # تمرير قيمة seconds هنا لكل مجموعة
         except Exception as e:
             await event.reply(f"⌔∮ لا يمكن العثور على المجموعة أو الدردشة {chat_username}: {str(e)}"
             )
@@ -49,22 +49,22 @@ async def Hussein(event):
     except BaseException:
         pass
     
-async def aljoker_allnshr(ANTi, sleeptimet, message):
+async def aljoker_allnshr(tt, sleeptimet, message):
     global yaAli
     yaAli = True
-    aljoker_chats = await ANTi.get_dialogs()
+    aljoker_chats = await tt.get_dialogs()
     while yaAli:
         for chat in aljoker_chats:
             if chat.is_group:
                 try:
                     if message.media:
-                        await ANTi.send_file(chat.id, message.media, caption=message.text)
+                        await tt.send_file(chat.id, message.media, caption=message.text)
                     else:
-                        await ANTi.send_message(chat.id, message.text)
+                        await tt.send_message(chat.id, message.text)
                 except Exception as e:
                     print(f"Error in sending message to chat {chat.id}: {e}")
         await asyncio.sleep(sleeptimet)
-@ANTi.on(events.NewMessage(outgoing=True, pattern=r"^\.نشر_كروبات (\d+)$"))
+@tt.on(events.NewMessage(outgoing=True, pattern=r"^\.نشر_كروبات (\d+)$"))
 async def Hussein(event):
     await event.delete()
     seconds = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
@@ -73,10 +73,10 @@ async def Hussein(event):
         sleeptimet = int(seconds[0])
     except Exception:
         return await event.reply("⌔∮ يجب استخدام كتابة صحيحة الرجاء التاكد من الامر اولا ⚠️")
-    ANTi = event.client
+    tt = event.client
     global yaAli
     yaAli = True
-    await aljoker_allnshr(ANTi, sleeptimet, message)
+    await aljoker_allnshr(tt, sleeptimet, message)
     joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
     joker = Get(joker)
     try:
@@ -84,23 +84,23 @@ async def Hussein(event):
     except BaseException:
         pass
 super_groups = ["super", "سوبر"]
-async def aljoker_supernshr(ANTi, sleeptimet, message):
+async def aljoker_supernshr(tt, sleeptimet, message):
     global yaAli
     yaAli = True
-    aljoker_chats = await ANTi.get_dialogs()
+    aljoker_chats = await tt.get_dialogs()
     while yaAli:
         for chat in aljoker_chats:
             chat_title_lower = chat.title.lower()
             if chat.is_group and any(keyword in chat_title_lower for keyword in super_groups):
                 try:
                     if message.media:
-                        await ANTi.send_file(chat.id, message.media, caption=message.text)
+                        await tt.send_file(chat.id, message.media, caption=message.text)
                     else:
-                        await ANTi.send_message(chat.id, message.text)
+                        await tt.send_message(chat.id, message.text)
                 except Exception as e:
                     print(f"Error in sending message to chat {chat.id}: {e}")
         await asyncio.sleep(sleeptimet)
-@ANTi.on(events.NewMessage(outgoing=True, pattern=r"^\.سوبر (\d+)$"))
+@tt.on(events.NewMessage(outgoing=True, pattern=r"^\.سوبر (\d+)$"))
 async def Hussein(event):
     await event.delete()
     seconds = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
@@ -109,22 +109,22 @@ async def Hussein(event):
         sleeptimet = int(seconds[0])
     except Exception:
         return await event.reply("⌔∮ يجب استخدام كتابة صحيحة الرجاء التاكد من الامر اولا ⚠️")
-    ANTi = event.client
+    tt = event.client
     global yaAli
     yaAli = True
-    await aljoker_supernshr(ANTi, sleeptimet, message)
+    await aljoker_supernshr(tt, sleeptimet, message)
     joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
     joker = Get(joker)
     try:
         await event.client(joker)
     except BaseException:
         pass
-@ANTi.on(events.NewMessage(outgoing=True, pattern='.ايقاف النشر'))
+@tt.on(events.NewMessage(outgoing=True, pattern='.ايقاف النشر'))
 async def stop_aljoker(event):
     global yaAli
     yaAli = False
     await event.edit("**᯽︙ تم ايقاف النشر التلقائي بنجاح ✓** ")
-@ANTi.on(events.NewMessage(outgoing=True, pattern=r"^\.(الاوامر|فحص)$"))
+@tt.on(events.NewMessage(outgoing=True, pattern=r"^\.(الاوامر|فحص)$"))
 async def Hussein(event):
     await event.delete()
     if event.pattern_match.group(1) == "الاوامر":
@@ -147,7 +147,7 @@ async def Hussein(event):
 
 • مُـلاحظة : جميع الأوامر اعلاه تستخدم بالرد على الرسالة او الكليشة المُراد نشرها
 
-===== ANTi =====
+===== tt =====
     **"""
         await event.reply(file='https://telegra.ph/file/a9ab192d3196e014ee015.jpg', message=joker_313)
     elif event.pattern_match.group(1) == "فحص":
@@ -159,5 +159,5 @@ async def Hussein(event):
             await event.client(joker)
         except BaseException:
             pass
-print('تم تشغيل نشر التلقائي لسورس ANTi')
-ANTi.run_until_disconnected()
+print('تم تشغيل نشر التلقائي لسورس tt')
+tt.run_until_disconnected()
